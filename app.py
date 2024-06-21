31257a4d63c1bde48049b7fbc4bd176f83d880d0
mthanhlm <mthanhlam111@gmail.com>
2024-06-21 23:46:43 +0700

    init

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods = ["GET"])
+def hello_word():
+    return "Hello Word"
+
+
+
+
+if __name__ == '__main__':
+    app.run(port=3000, debug=True)
+    
